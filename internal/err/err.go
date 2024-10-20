4e4926f90d1ba6f50957762071850a4c854a0528
Kazuya Saso <kazsan0215@gmail.com>
2024-10-20 12:07:31 +0900

    add internal error

---
+++ internal/err/err.go
@@ -0,0 +1,13 @@
+package err
+
+type InternalError struct {
+	message string
+}
+
+func (e *InternalError) Error() string {
+	return e.message
+}
+
+func NewInternalError(message string) *InternalError {
+	return &InternalError{message: message}
+}
